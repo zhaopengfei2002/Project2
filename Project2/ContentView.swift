1949afc99ce0d66a28806748c7ba4eb6a6f61a0f
Pengfei Zhao <zhaopengfei@gmail.com>
2022-09-21 16:53:00 -0400

    test commit

--- Project2/ContentView.swift
+++ Project2/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Text("My test Swift UI Project")
         }
         .padding()
     }
