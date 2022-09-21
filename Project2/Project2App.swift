7224b7dbbaaab6d647508a9dd21da0952fc58d2c
Pengfei Zhao <zhaopengfei@gmail.com>
2022-09-21 16:49:33 -0400

    Initial Commit

---
+++ Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by PENGFEI ZHAO on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
